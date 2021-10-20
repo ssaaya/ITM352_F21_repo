6f02fb63429a276049369cb43a0c90e3c57eb56f
saayah <saayah@hawaii.edu>
2021-10-19 17:58:24 -1000

    1019

---
+++ Labs/Lab11/ex2.js
@@ -0,0 +1,3 @@
+var attributes = "Saaya;19;MIS";
+
+console.log(attributes.split(';'));
---
+++ Quizzes/functinos.js
@@ -0,0 +1,3 @@
+var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+console.log('letters f through p in the alphabet are ' + alphabet.substring(5,16))
