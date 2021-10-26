571c5e03412b4a81d90be624a58993018f5ab96a
saayah <saayah@hawaii.edu>
2021-10-25 17:03:59 -1000

    102521

--- Labs/Lab11/ex2.js
+++ Labs/Lab11/ex2.js
@@ -1,6 +1,6 @@
 var attributes = "Saaya;19;19.5;-19.5";
 
-console.log(attributes.split(';',2));
+var parts = attributes.split(';');
 
 for(part of parts) {
     console.log(part, typeof part);
--- Labs/Lab11/ex4.js
+++ Labs/Lab11/ex4.js
@@ -1,7 +1,26 @@
-function isNonNegInt (q) {
+
+var attributes = "Saaya;19;19.5;-19.5";
+
+var parts = attributes.split(';');
+
+/*
+for(part of parts){
+
+    console.log(`${part} isNonNegInt: ${isNonNegInt(part)}`);
+}
+*/
+parts.forEach(checkIt)
+
+function isNonNegInt (q,returnErrors) {
+    // Checks if string q is a non negative integer. Returns true if q is a non neg integer
 errors = []; // assume no errors at first
 if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
 if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
 if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-return (errors.length == 0)
+return returnErrors ? Errors : (errors.length == 0)
+
+}
+
+function checkIt(item, index) {
+    console.log(`part ${index} is ${(isNonNegInt(item)?'a':'not a')} quantity`);
 }
