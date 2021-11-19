c2525a717734b95a69f8284021ce634603117a70
saayah <saayah@hawaii.edu>
2021-11-19 02:10:36 -1000

    1118

--- Assignments/Honjiyo_Saaya_Assignment1/server.js
+++ Assignments/Honjiyo_Saaya_Assignment1/server.js
@@ -1,5 +1,14 @@
 var express = require('express');
-var app = express();
 
-app.use(express.static('.'));
-app.listen(8080, () => console.log(`listening on port 8080`));
+var app = express();
+// get products data
+
+app.all('*', function (req, res, next) {
+    console.log(req.method, req.path);
+    next();
+} );
+
+
+app.use(express.public('./public'));
+
+var listener = app.listen(8080, () => { console.log('server started listening on port ' + listener.address().port) });
