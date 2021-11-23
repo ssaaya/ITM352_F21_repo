c2c3064a4ae6097909e398459a4a58f15287a70c
saayah <saayah@hawaii.edu>
2021-11-22 20:20:19 -1000

    1122

--- Assignments/Honjiyo_Saaya_Assignment1/server.js
+++ Assignments/Honjiyo_Saaya_Assignment1/server.js
@@ -1,16 +1,26 @@
 // From from Lab 13, screencast, uses npm express, querystring, and nodemon to run server
 
 // Pull data from product_data.js
-var data = require('./public/product_data.js');
+var data = require('./product_data');
 var products = data.products;
 // Sets query_string to load
-const query_string = require('query-string');
+const qs = require('query-string');
 // Allowing express, server starts
 var express = require('express');
 var app = express();
 // Loads parser
 var myParser = require("body-parser");
-
+// Monitors all requests
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to' + request.path);
+    next();
+});
+//Get request for products data
+app.get('/products.js', function (request, response) {
+    response.type('.js');
+    var products_str = `var products = ${JSON.stringify(products)};`;
+    response.send(products_str);
+});
 // From Lab 13, redirect invoice
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to path ' + request.path);
@@ -21,7 +31,7 @@ app.all('*', function (request, response, next) {
 app.use(express.urlencoded({extended: true}));
 
 // post to invoice
-app.post('/process_purchase', function (request, response) {
+app.post('/process_form', function (request, response) {
     // Grabs body
     let POST = request.body;
     if (typeof POST['submit_purchase'] != 'undefined') {
@@ -29,14 +39,25 @@ app.post('/process_purchase', function (request, response) {
         var has_quantity = false;
         for (i = 0; i < products.length; i++) {
             // Checks quantity
-            qty = POST[`quantity${i}`];
+            q = POST[`quantity${i}`];
             // Greater than 0
-            has_quantity = has_quantity || qty > 0;
+            has_quantity = has_quantity || q > 0;
             // Greater than 0, and valid using nonnegint function
-            has_valid_quantity = has_valid_quantity && isNonNegInt(qty);
+            has_valid_quantity = has_valid_quantity && isNonNegInt(q);
         }
         // Makes data into strings
         const stringified = query_string.stringify(POST);
+           
+   let qty_obj = {"quantity": JSON.stringify(request.body.quantity)};
+   if(Object.keys(errors).length === 0) {
+      
+
+    //If data is valid, create invoice
+    response.redirect('./invoice.html?' + qs.stringify(qty_obj));
+   } else {
+    qty_obj.errors = JSON.stringify(errors);
+    response.redirect('./pointeshoes.html?' + qs.stringify(qty_obj));
+   }
         // If valid
         if (has_valid_quantity && has_quantity) {
             // Sends to invoice
@@ -44,27 +65,28 @@ app.post('/process_purchase', function (request, response) {
             
         } else {
             // If not valid, sends back
-            response.redirect("./product_page.html?" + stringified);
+            response.redirect("./pointeshoes.html.html?" + stringified);
             
         }
    }
 });
 
-// Using express to access "Public" folder files
-app.use(express.static('./Public'));
-
-// Server start, listening to port 8080
-app.listen(8080, function () {
-    console.log(`listening on port 8080`)
-    }
-); // note the use of an anonymous function here
 
 // From lab 12
-function isNonNegInt(qty, return_errors = false) { //check if values are postitive, integer, whole values 
+function isNonNegInt(q, return_errors = false) { //check if values are postitive, integer, whole values 
     errors = []; // assume no errors at first
-    if (qty == '') qty = 0; //sets input quatity as 0 
-    if (Number(qty) != qty) errors.push(' <b>This is not a number!</b>'); // Check if string is a number value
-    else if (qty < 0) errors.push('<b>Negative value!</b>'); // Check if it is non-negative
-    else if (parseInt(qty) != qty) errors.push('<b>This is not a full value!</b>'); // Check that it is an integer
+    if (q == '') q = 0; //sets input quatity as 0 
+    if (Number(q) != q) errors.push(' <b>This is not a number!</b>'); // Check if string is a number value
+    else if (q < 0) errors.push('<b>Negative value!</b>'); // Check if it is non-negative
+    else if (parseInt(q) != q) errors.push('<b>This is not a full value!</b>'); // Check that it is an integer
     return return_errors ? errors : (errors.length == 0);
 }
+
+
+// Using express to access "Public" folder files
+app.use(express.static('./public'));
+
+// Server start, listening to port 8080
+app.listen(8080,() => console.log(`listening on port 8080`));
+    
+ // note the use of an anonymous function here
